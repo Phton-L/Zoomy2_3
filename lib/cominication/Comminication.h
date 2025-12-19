49b36b98ae749b1e7f653d332c466a8ae1cbcdc6
Phton-L <vinzent.Schillinger@outlook.com>
2025-12-19 18:47:32 +0100

    Comminication h version 1

--- lib/cominication/Comminication.h
+++ lib/cominication/Comminication.h
@@ -6,6 +6,23 @@ date: 17.12.2025
 #ifndef COMMINICATION_H
 #define COMMINICATION_H
 
+#include <Arduino.h>
 
+class Comminication
+{
+    private:
+        byte _start;
+        byte _end;
+        int _bout;
+        boolean _seril1Bol;
+    public:
+        void begin(byte start,byte end,int bout,boolean serial1Bol);
+
+        void read(int &direktionSend,int &speedSend);
+
+        void send(int direktionSend,int speedSend);
+
+
+};
 
 #endif
